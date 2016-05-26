903bcbc894c6d15623900948d18e0977f7680515
Rhys Powell <rhys.g.powell@gmail.com>
2016-05-25 20:02:15 -0400

    Initial Commit

---
+++ TestProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestProject
+//
+//  Created by Rhys Powell on 5/25/16.
+//  Copyright © 2016 Rhys Powell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
